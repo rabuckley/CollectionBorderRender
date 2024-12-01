ec722fa41de0630f946f46c9b87b2c273a17d56e
Ryan Buckley <ryan@brownthwaite.com>
2024-12-01 11:43:19 +0000

    Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectionBorderRender;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace CollectionBorderRender;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Items = [];
+
+		for (int i = 0; i < 25; i++)
+		{
+			Items.Add($"Item {i}");
+		}
+
+		BindingContext = this;
+	}
+
+	public List<string> Items { get; }
+}
+
